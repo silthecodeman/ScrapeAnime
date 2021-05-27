a2c361949a8a617c6ae4dd9f3f317de86973b408
silthecodeman <68767768+silthecodeman@users.noreply.github.com>
2021-05-26 20:30:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import orginizeShows
 import getShows
+import requests
+import pandas
 import time
 import os
+import re
 
 if __name__ == "__main__":
 	pathelem = [i for i in os.getcwd()[1:].split('/')]
-	shows = getShows.showsFinder('/{}/{}/Documents/geckodrivermac/chromedriver'.format(pathelem[0], pathelem[1]), 64)
+
+	shows = getShows.showsFinder('/{}/{}/Documents/geckodrivermac/chromedriver'.format(pathelem[0], pathelem[1]), 1)
 	result = shows.run('completed')
-	print(result)
+
+	df = orginizeShows.cleanShowUrls(result)
+	
+	print(df)
