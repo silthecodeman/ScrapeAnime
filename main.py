fc07afe9d7c5e963571bc6a9cd29ad724e3e6485
silthecodeman <68767768+silthecodeman@users.noreply.github.com>
2021-05-28 12:58:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.chrome.options import Options
+from dataScrapeFunctions import downloadEpisode, cleanShowUrls, getEpisodesDF
 from bs4 import BeautifulSoup
-import orginizeShows
-import getShows
 import requests
 import pandas
 import time
@@ -18,10 +17,33 @@ import re
 
 if __name__ == "__main__":
 	pathelem = [i for i in os.getcwd()[1:].split('/')]
+	driverPath = '/{}/{}/Documents/geckodrivermac/chromedriver'.format(pathelem[0], pathelem[1])
 
-	shows = getShows.showsFinder('/{}/{}/Documents/geckodrivermac/chromedriver'.format(pathelem[0], pathelem[1]), 1)
-	result = shows.run('completed')
+	Done = True 
 
-	df = orginizeShows.cleanShowUrls(result)
+	listOfUrls = [input("Anime URL >> ")]
+
+	while Done:
+		more = input("Are there any more show urls? [Y/n] ")
+
+		if more == 'Y' or more == 'y':
+			listOfUrls.append(input("Anime URL >> "))
+		elif more == 'N' or more == 'n':
+			Done = False
+		else:
+			print('Invalid Input')
+
+		time.sleep(1)
+
+	df = cleanShowUrls(listOfUrls)
 	
-	print(df)
+	df = getEpisodesDF(df)
+
+	for index, row in df.iterrows():
+		animeName = '"{}"'.format(row['Title'].replace('/',' ').replace('.', ''))
+		print(animeName)
+		os.system("mkdir Animes/{}".format(animeName))
+		lst = str(row['Episodes']).replace('[','').replace(']', '').replace("'", "").split(', ')[::-1]
+		
+		for i in range(len(lst)):
+			downloadEpisode(lst[i], driverPath, 'Animes/{}/Episode{}.mp4'.format(animeName, (i+1)))
